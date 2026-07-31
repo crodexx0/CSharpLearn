45e2f65862fbe0c63edd58a415d35f9c46e13ef4
crodexx0 <adrianhabagat13@gmail.com>
2026-07-31 08:02:50 +0800

    initial commit

---
+++ c#Logic/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+string pangram2 = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram2.Contains("fox"));
+Console.WriteLine(!pangram2.Contains("cow"));
+int a = 7;
+int b = 6;
+Console.WriteLine(a != b); // output: True
+string s1 = "Hello";
+string s2 = "Hello";
+Console.WriteLine(s1 != s2); // output: False
+*/
+
+
+
+/* 
+CONDITIONAL OPERATOR
+<evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+
+int saleAmount = 1001;
+//int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+*/
+
+
+/*
+//Code challenge: display the result of a coin flip
+Random coin = new Random();
+int result = coin.Next(2);
+Console.WriteLine(result);
+Console.WriteLine($"Coin Toss Result: {(result == 1 ? "Heads" : "Tails" )}");
+Random coin = new Random();
+Console.WriteLine($"Coin Toss Result: {(coin.Next(2) == 1 ? "Heads" : "Tails")}");
+*/
+
+
+
+string permission = "Admin|Manager";
+int level = 20;
+Console.WriteLine((permission.Contains("Admin") && level > 55) ? "Welcome, Super Admin user." : (permission.Contains("Admin") && level <= 55) ? "Welcome, Admin user." : (permission.Contains("Manager") && level >= 20) ? "Contact an Admin for access." : (permission.Contains("Manager") && level < 20) ? "You do not have sufficient privileges." : "You do not have sufficient privileges.");
---
+++ simpleC#Application/Program.cs
@@ -0,0 +1,75 @@
+﻿/*string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+//fraudulentOrderIDs[3] = "D000";
+
+string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fradulent orders to process.");
+*/
+
+
+
+/*int[] inventory = [200, 450, 700, 175, 250];
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+  bin++;
+  sum += items;
+  Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
+*/
+
+
+
+/*string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+foreach (string orderID in orderIDs)
+{
+  if (orderID.StartsWith("B"))
+  {
+    Console.WriteLine(orderID);
+  }
+}*/
+
+
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+//Loop through each blank orderID
+for (int i=0; i<orderIDs.Length; i++)
+{
+  // Get a random value that equates to ASCII letters A through E
+  int prefixValue = random.Next(65, 70);
+  // Convert the random value into a char, then a string
+  string prefix = Convert.ToChar(prefixValue).ToString();
+  // Create a random number, pad with zeroes
+  string suffix = random.Next(1, 1000).ToString("000");
+  // Combine the prefix and suffix together, then assign to current OrderID
+  orderIDs[i] = prefix + suffix;
+}
+
+// Print out each orderID
+foreach (var orderID in orderIDs)
+{
+  Console.WriteLine(orderID);
+}
