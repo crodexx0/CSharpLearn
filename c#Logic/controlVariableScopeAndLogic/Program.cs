8beb77db8fabc837b7d559730896a46bac8b30ca
crodexx0 <adrianhabagat13@gmail.com>
2026-08-03 06:18:11 +0800

    Control Variable Scope and Logic - DONE. Remove code blocks from If Statements - Start

--- c#Logic/controlVariableScopeAndLogic/Program.cs
+++ c#Logic/controlVariableScopeAndLogic/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
+*/
+
+
+
+/*
+This code displays error because if statements checks whether flag is true or false.
+
+// Code sample 1
+bool flag = true;
+int value;
+
+if (flag)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
+*/
+
+
+
+//This code doesnt display error even though value was initialized inside the if statement because condition is always true making the if statement like it wasn't even there
+int value;
+
+if (true)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
