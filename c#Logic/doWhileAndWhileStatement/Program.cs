0ac6c0ee32158add4c232c7894716690dcac5266
Adrian <adrianhabagat13@gmail.com>
2026-08-05 20:35:05 +0800

    Add looping logic using the do-while and while statements - CONTINUE

--- c#Logic/doWhileAndWhileStatement/Program.cs
+++ c#Logic/doWhileAndWhileStatement/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+//This code uses a `do-while` loop to generate random numbers between 1 and 10 until the number 7 is generated.
+Random random = new Random();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+*/
+
+/*
+// This generates and prints random numbers between 1 and 10 until a number less than 3 is generated.
+Random random = new Random();
+int current = random.Next(1, 11);
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+*/
+
+// This code generates random numbers between 1 and 10, printing them unless the number is 8 or higher, and continues until the number 7 is generated.
+Random random = new Random();
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
