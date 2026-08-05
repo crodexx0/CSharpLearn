dddbe27fc8793c1eb77223249334cc5ec0eafc3c
crodexx0 <adrianhabagat13@gmail.com>
2026-08-06 07:51:54 +0800

    Add looping logic using the do-while and while statements - CONTINUE 2

---
+++ TestPoject/OJTAttendanceApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OJTAttendanceApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OJTAttendanceApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d37a02e-78b9-4b96-aa5d-2147d4437dc8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- c#Logic/doWhileAndWhileStatement/Program.cs
+++ c#Logic/doWhileAndWhileStatement/Program.cs
@@ -35,3 +35,34 @@ do
 
     Console.WriteLine(current);
 } while (current != 7);
+
+/* 
+CHALLENGE ACTIVITY: implement the game rules
+
+You must use either the do-while statement or the while statement as an outer game loop.
+The hero and the monster start with 10 health points.
+All attacks are a value between 1 and 10.
+The hero attacks first.
+Print the amount of health the monster lost and their remaining health.
+If the monster's health is greater than 0, it can attack the hero.
+Print the amount of health the hero lost and their remaining health.
+Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
+Print the winner.
+*/
+
+int monsterHealth = 10;
+int heroHealth = 10;
+
+int monsterDamage = 0;
+int heroDamage = 0;
+
+do
+{
+    monsterDamage = random.Next(1, 11);
+    heroDamage = random.Next(1, 11);
+
+    Console.WriteLine($"Monster was damaged and lost {heroDamage} health and now has {monsterHealth -= heroDamage} health.");
+    if (monsterHealth <= 0) continue;
+    Console.WriteLine($"Hero was damaged and lost {monsterDamage} health and now has {heroHealth -= monsterDamage} health.");
+} while (heroHealth > 0 && monsterHealth > 0);
+Console.WriteLine(heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!");
